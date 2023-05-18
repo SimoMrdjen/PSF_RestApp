7abc943fe48458246fc553be1bd53de570e7290c
Simo Mrdjen <dr.sizni@gmail.com>
2023-05-18 10:49:26 +0200

    added +r...

---
+++ src/main/java/psf/ucitavanje/obrazaca/obrazac5/Obr5Controller.java
@@ -0,0 +1,23 @@
+package psf.ucitavanje.obrazaca.obrazac5;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/obrazac5")
+@RequiredArgsConstructor
+public class Obr5Controller {
+
+    private final IObr5Service service;
+
+    @PostMapping
+    public List<Obrazac5DTO> saveAll(@RequestBody List<Obrazac5DTO> dtos) {
+        return service.saveAll(dtos);
+    }
+
+}
