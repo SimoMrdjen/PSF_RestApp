126c1f966c83b469a28c462ca4eaccfc2a7d4472
Simo Mrdjen <dr.sizni@gmail.com>
2023-07-14 09:07:48 +0200

    before new branch

--- src/frontend/src/components/ObrazacIOButton.js
+++ src/frontend/src/components/ObrazacIOButton.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import * as XLSX from 'xlsx';
-import { saveObrazacIO } from '../client';
-import { IndividualData } from './IndividualData';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Dropdown, Space, Tooltip, message } from 'antd';
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+import { saveObrazacIO } from "../client";
+import { IndividualData } from "./IndividualData";
+import { DownOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Dropdown, Space, Tooltip, message } from "antd";
 
 function ObrazacIOButton({ kvartal, setKvartal, access_token }) {
   const [excelFile, setExcelFile] = useState(null);
@@ -14,8 +14,9 @@ function ObrazacIOButton({ kvartal, setKvartal, access_token }) {
     let selectedFile = e.target.files[0];
     if (selectedFile) {
       if (
-        selectedFile.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ||
-        selectedFile.name.endsWith('.xls')
+        selectedFile.type ===
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+        selectedFile.name.endsWith(".xls")
       ) {
         let reader = new FileReader();
         reader.readAsArrayBuffer(selectedFile);
@@ -25,55 +26,56 @@ function ObrazacIOButton({ kvartal, setKvartal, access_token }) {
         };
       } else {
         console.log(selectedFile.type);
-        setExcelFileError('Izabrani dokument nije XLSX ili XLS!');
+        setExcelFileError("Izabrani dokument nije XLSX ili XLS!");
         setExcelFile(null);
       }
     } else {
-      console.log('Plz select your file');
+      console.log("Plz select your file");
     }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (excelFile !== null) {
-      const workbook = XLSX.read(excelFile, { type: 'buffer' });
+      const workbook = XLSX.read(excelFile, { type: "buffer" });
       const worksheetName = workbook.SheetNames[0];
       const worksheet = workbook.Sheets[worksheetName];
       const jsonData = XLSX.utils.sheet_to_json(worksheet, {
         header: 1,
         range: 6,
       });
-       const filteredData = jsonData.filter((row) => {
-          const firstProperty = row[0];
-      return typeof firstProperty !== 'string'
-          && typeof firstProperty !== 'undefined';
-         });
+      const filteredData = jsonData.filter((row) => {
+        const firstProperty = row[0];
+        return (
+          typeof firstProperty !== "string" &&
+          typeof firstProperty !== "undefined"
+        );
+      });
 
       const headers = filteredData[0];
       const data = filteredData.slice(1).map((row) => {
         let obj = {};
 
-      headers.slice(0, 7).forEach((header, index) => {
-        let value;
-        if (index === 3 || index === 4) {
-          value = String(row[index]).padStart(4, '0');
-        }
-        else if (index === 1 ) {
-          value = String(row[index]).padStart(3, '0');
-        } else {
-          value = row[index];
-        }
-        obj[`prop${index + 1}`] = value;
-      });
+        headers.slice(0, 7).forEach((header, index) => {
+          let value;
+          if (index === 3 || index === 4) {
+            value = String(row[index]).padStart(4, "0");
+          } else if (index === 1) {
+            value = String(row[index]).padStart(3, "0");
+          } else {
+            value = row[index];
+          }
+          obj[`prop${index + 1}`] = value;
+        });
         return obj;
       });
-       const jbbk = worksheet['B3']?.v || '';
-       const year = worksheet['D3']?.v || '';
+      const jbbk = worksheet["B3"]?.v || "";
+      const year = worksheet["D3"]?.v || "";
 
-       console.log(jbbk);
-       //data.splice(116,1000);
+      console.log(jbbk);
+      //data.splice(116,1000);
       console.log(data);
-      saveObrazacIO(data, kvartal, year, access_token );
+      saveObrazacIO(data, kvartal, year, access_token);
       setExcelData(JSON.stringify(data, null, 4));
     } else {
       setExcelData(null);
@@ -87,13 +89,22 @@ function ObrazacIOButton({ kvartal, setKvartal, access_token }) {
           <h5>Izaberi ObrazacIO</h5>
         </label>
         <br></br>
-        <input type="file" className="form-control" onChange={handleFile} required></input>
+        <input
+          type="file"
+          className="form-control"
+          onChange={handleFile}
+          required
+        ></input>
         {excelFileError && (
-          <div className="text-danger" style={{ marginTop: 5 + 'px' }}>
+          <div className="text-danger" style={{ marginTop: 5 + "px" }}>
             {excelFileError}
           </div>
         )}
-        <button type="submit" className="btn btn-primary" style={{ marginTop: 15 + 'px' }}>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          style={{ marginTop: 15 + "px" }}
+        >
           Učitaj ObrazacIO
         </button>
       </form>
@@ -115,5 +126,3 @@ function ObrazacIOButton({ kvartal, setKvartal, access_token }) {
 }
 
 export default ObrazacIOButton;
-
-
